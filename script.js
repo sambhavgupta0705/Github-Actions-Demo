51d51504e3874c42e5cc4a825502bc2d5e266e6d
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-03-25 16:59:53 +0530

    fix

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 console.log("Hello")
 var a=10;
 console.log(a);
+console.log("da")
+console.log("da")
+console.log("da")
+console.log("da")
+console.log("da")
 
-console.log("this is weeeeeeeea test ")
-// console.log("hello all")
+
+
 
